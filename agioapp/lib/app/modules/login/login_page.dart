4de1cb2a2eb8c437d4af792234ef85081cc31b5f
raphaelbarbosaqwerty <raphaelcorreabarbosa@hotmail.com>
2021-10-02 03:40:25 -0300

    Login middle

--- agioapp/lib/app/modules/login/login_page.dart
+++ agioapp/lib/app/modules/login/login_page.dart
@@ -8,17 +8,94 @@ class LoginPage extends StatefulWidget {
   @override
   LoginPageState createState() => LoginPageState();
 }
+
 class LoginPageState extends State<LoginPage> {
   final LoginStore store = Modular.get();
 
   @override
   Widget build(BuildContext context) {
+    var textTheme = Theme.of(context).textTheme;
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Column(
-        children: <Widget>[],
+      appBar: AppBar(),
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              'Welcome Back!',
+              style: textTheme.bodyText1?.copyWith(
+                fontSize: 24,
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              'Please sign in to your account',
+              style: textTheme.bodyText2?.copyWith(),
+            ),
+            const SizedBox(
+              height: 24,
+            ),
+            TextFormField(
+              controller: TextEditingController(),
+              style: textTheme.bodyText1?.copyWith(
+                color: Colors.white,
+              ),
+              decoration: InputDecoration(
+                hintText: 'Email',
+              ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            TextFormField(
+              controller: TextEditingController(),
+              style: textTheme.bodyText1?.copyWith(
+                color: Colors.white,
+              ),
+              obscureText: true,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                suffixIcon: Icon(
+                  Icons.remove_red_eye,
+                ),
+              ),
+            ),
+            const SizedBox(
+              height: 8,
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: Text(
+                'Forgot Password?',
+                style: textTheme.bodyText2?.copyWith(),
+              ),
+            ),
+            const SizedBox(
+              height: 80,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 70,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Sign in'),
+              ),
+            ),
+            const SizedBox(
+              height: 12,
+            ),
+            SizedBox(
+              width: double.infinity,
+              height: 70,
+              child: ElevatedButton(
+                onPressed: () {},
+                child: Text('Sign in'),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
--- agioapp/lib/app/utils/atomic/atomic.dart
+++ agioapp/lib/app/utils/atomic/atomic.dart
@@ -1,9 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Atomic {
-  static Color? _nightBlack = Color(0xFF171821);
-  static Color? red = Color(0xFFee786b);
+  static Color _nightBlack = Color(0xFF171821);
+  static Color _purple = Color(0xFF5568fe);
+  static Color _red = Color(0xFFee786b);
+
+  static Color _subtitle = Color(0xFF6b6f77);
+  static Color _formField = Color(0xFF252a34);
 
   static ThemeData buildTheme(BuildContext context) {
     return ThemeData(
@@ -12,8 +17,48 @@ class Atomic {
       ),
       scaffoldBackgroundColor: _nightBlack,
       primaryColor: _nightBlack, //FF2c2d33
+      colorScheme: ThemeData().colorScheme.copyWith(
+            secondary: Colors.red,
+          ),
+      elevatedButtonTheme: ElevatedButtonThemeData(
+        style: ElevatedButton.styleFrom(
+          primary: _purple,
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+        ),
+      ),
       primarySwatch: Colors.blue,
-      textTheme: TextTheme(),
+      iconTheme: IconThemeData(
+        color: _subtitle,
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+          fillColor: _formField,
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+              color: _purple,
+            ),
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          filled: true,
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(20.0),
+          ),
+          hintStyle: TextStyle(
+            color: _subtitle,
+          )),
+      textTheme: TextTheme(
+        bodyText1: GoogleFonts.roboto(
+          textStyle: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        bodyText2: GoogleFonts.roboto(
+          textStyle: TextStyle(
+            color: _subtitle,
+          ),
+        ),
+      ),
     );
   }
 }
