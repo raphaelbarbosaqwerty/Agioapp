31937eb917aa19f754dbc782b7bae9087241da1c
raphaelbarbosaqwerty <raphaelcorreabarbosa@hotmail.com>
2021-10-02 01:51:42 -0300

    Login module routes

--- agioapp/lib/app/app_module.dart
+++ agioapp/lib/app/app_module.dart
@@ -1,3 +1,4 @@
+import 'package:agioapp/app/modules/login/login_module.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
 import 'modules/home/home_module.dart';
@@ -8,7 +9,7 @@ class AppModule extends Module {
 
   @override
   final List<ModularRoute> routes = [
-    ModuleRoute(Modular.initialRoute, module: HomeModule()),
+    ModuleRoute(Modular.initialRoute, module: LoginModule()),
+    ModuleRoute('/home', module: HomeModule()),
   ];
-
 }
