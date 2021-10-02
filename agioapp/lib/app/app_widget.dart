c800548ae8b87ba02e7831c446aac56155a58073
raphaelbarbosaqwerty <raphaelcorreabarbosa@hotmail.com>
2021-10-02 01:41:07 -0300

    Setup atomic design system

--- agioapp/lib/app/app_widget.dart
+++ agioapp/lib/app/app_widget.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
 
+import 'utils/atomic/atomic.dart';
+
 class AppWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Slidy',
-      theme: ThemeData(primarySwatch: Colors.blue),
+      theme: Atomic.buildTheme(context),
     ).modular();
   }
 }
---
+++ agioapp/lib/app/utils/atomic/atomic.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class Atomic {
+  static ThemeData buildTheme(BuildContext context) {
+    return ThemeData(
+      primarySwatch: Colors.blue,
+      textTheme: TextTheme(),
+    );
+  }
+}
