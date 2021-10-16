c9d4c81efd83f17950dd2b26946c6f326ea92783
raphaelbarbosaqwerty <raphaelcorreabarbosa@hotmail.com>
2021-10-15 23:57:31 -0300

    Fixing space

--- agioapp/lib/app/modules/register/register_page.dart
+++ agioapp/lib/app/modules/register/register_page.dart
@@ -13,91 +13,134 @@ class RegisterPage extends StatefulWidget {
 class RegisterPageState extends State<RegisterPage> {
   final RegisterStore store = Modular.get();
 
+  Future<bool> _showMyDialog() async {
+    var response = await showDialog(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('AlertDialog Title'),
+          content: SingleChildScrollView(
+            child: ListBody(
+              children: const <Widget>[
+                Text('This is a demo alert dialog.'),
+                Text('Would you like to approve of this message?'),
+              ],
+            ),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Approve'),
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+            ),
+            TextButton(
+              child: const Text(
+                'Cancel',
+                style: TextStyle(color: Colors.red),
+              ),
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+            ),
+          ],
+        );
+      },
+    );
+    return response;
+  }
+
   @override
   Widget build(BuildContext context) {
     var textTheme = Theme.of(context).textTheme;
-    return Scaffold(
-      appBar: AppBar(),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 24.0),
-        child: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              children: <Widget>[
-                Text(
-                  'Create new account',
-                  style: textTheme.bodyText1?.copyWith(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 24,
-                  ),
-                ),
-                const SizedBox(
-                  height: 18,
-                ),
-                Text(
-                  'Please fill the form to continue',
-                  style: textTheme.bodyText2?.copyWith(),
-                ),
-                const SizedBox(
-                  height: 74,
-                ),
-                TextFormField(
-                  controller: TextEditingController(),
-                  style: textTheme.bodyText1?.copyWith(
-                    color: Colors.white,
-                  ),
-                  decoration: InputDecoration(
-                    hintText: 'Full name',
-                  ),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextFormField(
-                  controller: TextEditingController(),
-                  style: textTheme.bodyText1?.copyWith(
-                    color: Colors.white,
-                  ),
-                  decoration: InputDecoration(
-                    hintText: 'Email Address',
-                  ),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextFormField(
-                  controller: TextEditingController(),
-                  style: textTheme.bodyText1?.copyWith(
-                    color: Colors.white,
-                  ),
-                  decoration: InputDecoration(
-                    hintText: 'Phone Number',
-                  ),
-                ),
-                const SizedBox(
-                  height: 16,
-                ),
-                TextFormField(
-                  controller: TextEditingController(),
-                  style: textTheme.bodyText1?.copyWith(
-                    color: Colors.white,
-                  ),
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    hintText: 'Password',
-                    suffixIcon: Icon(
-                      Icons.remove_red_eye,
+    return WillPopScope(
+      onWillPop: () async => _showMyDialog(),
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+        ),
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 24.0),
+          child: SingleChildScrollView(
+            child: Center(
+              child: Column(
+                children: <Widget>[
+                  Text(
+                    'Create new account',
+                    style: textTheme.bodyText1?.copyWith(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 24,
                     ),
                   ),
-                ),
-                const SizedBox(
-                  height: 80,
-                ),
-                TheButtonWidget(
-                  child: Text('Sign up'),
-                  onPressed: () {},
-                ),
-              ],
+                  const SizedBox(
+                    height: 18,
+                  ),
+                  Text(
+                    'Please fill the form to continue',
+                    style: textTheme.bodyText2?.copyWith(),
+                  ),
+                  const SizedBox(
+                    height: 74,
+                  ),
+                  TextFormField(
+                    controller: TextEditingController(),
+                    style: textTheme.bodyText1?.copyWith(
+                      color: Colors.white,
+                    ),
+                    decoration: InputDecoration(
+                      hintText: 'Full name',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: TextEditingController(),
+                    style: textTheme.bodyText1?.copyWith(
+                      color: Colors.white,
+                    ),
+                    decoration: InputDecoration(
+                      hintText: 'Email Address',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: TextEditingController(),
+                    style: textTheme.bodyText1?.copyWith(
+                      color: Colors.white,
+                    ),
+                    decoration: InputDecoration(
+                      hintText: 'Phone Number',
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  TextFormField(
+                    controller: TextEditingController(),
+                    style: textTheme.bodyText1?.copyWith(
+                      color: Colors.white,
+                    ),
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      suffixIcon: Icon(
+                        Icons.remove_red_eye,
+                      ),
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 80,
+                  ),
+                  TheButtonWidget(
+                    child: Text('Sign up'),
+                    onPressed: () {},
+                  ),
+                ],
+              ),
             ),
           ),
         ),
